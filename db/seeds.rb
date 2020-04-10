8d57e7f797ba86ed0672b887831b5792543fca59
Lisenenkov Dmitry <lisenenkov@mail.ru>
2020-04-10 23:16:30 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create([{ title: 'Math' }, { title: 'Programming' }, { title: 'Language' }])
+
+users = User.create([{ name: 'John' }, { name: 'Dick' }, { name: 'Mary' }])
+
+tests = Test.create([
+  { title: 'Arithmetic', level: 0, category_id: categories[0].id },
+  { title: 'Ruby', level: 1, category_id: categories[1].id },
+  { title: 'Russian', level: 2, category_id: categories[2].id }
+])
+
+questions = Question.create([
+  { body: '2 + 2 = ', test_id: tests[0].id },
+  { body: '4 + 2 = ', test_id: tests[0].id },
+  { body: '8 + 2 = ', test_id: tests[0].id }
+])
+
+answers = Answer.create([
+  { question_id: questions[0].id, user_id: users[1].id, correct: true },
+  { question_id: questions[1].id, user_id: users[1].id }
+])
