aa8f9c4f496664a694f8cc7fba197f3344105b1a
Lisenenkov Dmitry <lisenenkov@mail.ru>
2020-04-10 22:22:15 +0300

    added joins

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,11 @@
 class Test < ApplicationRecord
+
+def self.names_by_category(category_name)
+  Test.
+  joins('join categories on tests.category_id = categories.id').
+  where('categories.title LIKE :category_name', category_name: category_name).
+  order('tests.title desc').
+  pluck('tests.title')
+end
+
 end
