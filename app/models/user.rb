8d5d185bfb70db3e8fcba561cd56c4289176a5ed
Lisenenkov Dmitry <lisenenkov@mail.ru>
2020-04-11 19:57:20 +0300

    undo added joins

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,2 @@
 class User < ApplicationRecord
-
-def tests
-  Test.
-  joins('join questions on tests.id = questions.test_id').
-  joins('join answers on questions.id = answers.question_id').
-  where('answers.user_id = :user_id', user_id: id).
-  distinct  
-end
-
 end
