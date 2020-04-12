42af6f2745af6f39186ccf2ce5c7ad6f7320aa20
Lisenenkov Dmitry <lisenenkov@mail.ru>
2020-04-12 16:21:56 +0300

    added level parameter to get tests by user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
 
-def tests
+def tests_by_level(level)
   Test.
   joins('join results on tests.id = results.test_id').
-  where(results: { user_id: id }).
+  where(results: { user_id: id }, tests: { level: level }).
   distinct  
 end
 
