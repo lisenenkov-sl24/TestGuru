be4c4085ad4c7c40db319851426775c39559799f
Dmitriy Lisenenkov <lisenenkov.d@sl24leasing.ru>
2020-04-14 21:03:44 +0300

    dependent

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,15 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
   belongs_to :author, class_name: 'User'
-  has_many :questions
-  has_many :results
   has_many :users, through: :results
 
   def self.names_by_category(category_name)
-    joins('join categories on tests.category_id = categories.id').
-      where(categories: { title: category_name }).
-      order('tests.title desc').
-      pluck('tests.title')
+    Category.find_by(title: category_name)&.
+      tests&.order(title: :desc)&.pluck(:title)
   end
 
 end
