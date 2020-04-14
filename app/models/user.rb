80ef95b3644053ca678b2511a5e1bfb41460b6a2
Dmitriy Lisenenkov <lisenenkov.d@sl24leasing.ru>
2020-04-14 18:31:55 +0300

    added test author

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_many :results
-  has_many :tests, through: :results
- 
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :processed_tests, class_name: 'Test', through: :results, source: 'user'
 
   def tests_by_level(level)
-    tests.
+    processed_tests.
     where(level: level).
     distinct  
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_163055) do
+ActiveRecord::Schema.define(version: 2020_04_14_150128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_04_11_163055) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -67,4 +69,5 @@ ActiveRecord::Schema.define(version: 2020_04_11_163055) do
   add_foreign_key "results", "tests"
   add_foreign_key "results", "users"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
