61dc9386c8b386be2dbcacaf1d3f223650eb7332
Lisenenkov Dmitry <lisenenkov@mail.ru>
2020-04-15 08:57:47 +0300

    resorted associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: 'User'
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
-  belongs_to :author, class_name: 'User'
   has_many :users, through: :results
 
   def self.names_by_category(category_name)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :results, dependent: :destroy
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
+  has_many :results, dependent: :destroy
   has_many :processed_tests, class_name: 'Test', through: :results, source: 'user'
 
   def tests_by_level(level)
