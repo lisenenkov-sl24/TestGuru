0c52513ddaca542123dd6cab80f324386dd08ff6
Dmitriy Lisenenkov <lisenenkov.d@sl24leasing.ru>
2020-04-20 00:29:44 +0300

    Added count validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,15 @@
 class Answer < ApplicationRecord
   belongs_to :question
   validates :text, presence: true
+  validate :check_answers_count, on: :create
 
   scope :correct, -> { where correct: true }
+
+  private
+
+  def check_answers_count
+    if question.answers.count > 3
+      errors.add(:base, "Answers count must be between 1 and 4")
+    end
+  end
 end
