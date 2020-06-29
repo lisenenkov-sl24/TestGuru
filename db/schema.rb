c52a1d7e948fcbe9b2cdd4f0f1c6a1d0eb1695b0
Lisenenkov Dmitry <lisenenkov@mail.ru>
2020-06-29 18:13:03 +0300

    Timer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_144309) do
+ActiveRecord::Schema.define(version: 2020_06_28_124950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2020_06_25_144309) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
+    t.integer "time_limit", default: 0, null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["level", "title"], name: "index_tests_on_level_and_title", unique: true
